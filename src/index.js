74463aab6479232cde2258d40c31100b4c3754f8
Dan <danretegan@yahoo.com>
2024-02-25 18:40:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './index.css';
 import { Provider as ReduxProvider } from 'react-redux';
-import store from './redux/store.js';
+import persistor from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ReduxProvider store={store}>
+  <ReduxProvider store={persistor}>
     <App />
   </ReduxProvider>
 );
